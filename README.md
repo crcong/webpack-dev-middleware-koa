dca92b10718d029891fd497a35a266888c188f39
Ruicong <crcong@foxmail.com>
2022-03-18 18:32:18 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,12 @@
+# webpack-dev-middleware
+
+webpack-dev-middleware for koa
+
+## Usage
+
+```bash
+npm i webpack-dev-middleware-koa -D
+```
+
+same with https://github.com/webpack/webpack-dev-middleware
+
---
+++ src/index.js
@@ -0,0 +1,49 @@
+const expressMiddleware = require('webpack-dev-middleware')
+
+function middleware(doIt, req, res) {
+  const { end: originalEnd } = res
+
+  return new Promise((resolve) => {
+    res.end = function end(...args) {
+      originalEnd.apply(this, args)
+      resolve(0)
+    }
+    doIt(req, res, () => {
+      resolve(1)
+    })
+  })
+}
+
+module.exports = function(compiler, option) {
+  const doIt = expressMiddleware(compiler, option)
+
+  async function koaMiddleware(ctx, next) {
+    const { req } = ctx
+    const locals = ctx.locals || ctx.state
+
+    ctx.webpack = doIt
+
+    const runNext = await middleware(doIt, req, {
+      end(content) {
+        ctx.body = content
+      },
+      locals,
+      setHeader(...args) {
+        ctx.set(...args)
+      },
+      getHeader(header) {
+        return ctx.headers[header]
+      },
+    })
+
+    if (runNext) {
+      await next()
+    }
+  }
+
+  Object.keys(doIt).forEach((p) => {
+    koaMiddleware[p] = doIt[p]
+  })
+
+  return koaMiddleware
+}
